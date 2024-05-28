7b7e9226e1d9c09e066422b5fd8f1cf5cc7f142b
BookShelf <happy010627@gmail.com>
2024-05-28 18:50:59 +0900

    feat: Resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,9 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <h1>2024 실전코딩 2024.05.28</h1>
+      <p>1학기 수강중, 웹프 기억 살리는중</p>
+    </div>);
 };
 
 export default Resume;
